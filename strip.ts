6abd36d72e836788b978282d3d6074a7a42371f1
Kris Pomphrey <kris@tkf.network>
2024-05-26 18:44:57 +0100

    Cleanup strip  script

--- strip.ts
+++ strip.ts
@@ -1,18 +1,23 @@
-import data from "./cards/en/sv6.json" assert { type: "json" };
 import { writeFileSync } from "node:fs";
 import path from "node:path";
 
 const start = async () => {
-  console.log(path.resolve(__dirname, "./cards/en/sv6ee.json"));
-  const d = writeFileSync(
-    path.resolve(__dirname, "./cards/en/sv6ee.json"),
-    JSON.stringify(data.map(({ cardmarket, tcgplayer, set, ...item }) => item))
-  );
-
-  console.log(d);
+  for (const rawPath of ["cards/en/sv6.json"]) {
+    const filePath = path.resolve(__dirname, rawPath);
+    const { default: data } = await import(filePath);
+    writeFileSync(
+      filePath,
+      JSON.stringify(
+        data.map(({ cardmarket, tcgplayer, set, ...item }) => item)
+      )
+    );
+  }
   return;
 };
 
 start()
   .then(() => process.exit(0))
-  .catch(() => process.exit(1));
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
