1351fd0cf788aac406a7da6de8607db81168e13c
Kris Pomphrey <kris@tkf.network>
2025-06-01 22:26:07 +0100

    chore: add ability to scrape, scrape svp

---
+++ scripts/scrape-set.ts
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+import { writeFile } from "fs/promises";
+import path from "path";
+
+const [setId] = process.argv.slice(2);
+
+if (!setId) {
+  console.error("SetId required");
+  process.exit(1);
+}
+
+dotenv.config({
+  path: path.resolve(__dirname, ".env"),
+});
+
+const get = (endpoint: string) => {
+  const url = [process.env.API_URL, endpoint].join("");
+  console.log(url);
+  return fetch(url, {
+    headers: { "x-Api-Key": String(process.env.API_KEY) },
+  }).then((response) => response.json());
+};
+
+const scrape = async (setId: string) => {
+  let page = 0;
+  let pages = 1;
+  const cards: any[] = [];
+
+  while (page < pages) {
+    const iteration = page || 1;
+    const data = (await get(
+      ["/cards", `q=set.id:${setId}&page=${iteration}`].join("?")
+    )) as { data: any[]; page: number; count: number; pageSize: number };
+
+    console.log(`Page: ${iteration} of ${pages}`);
+
+    pages = Math.ceil(data.count / data.pageSize);
+    page = data.page;
+
+    cards.push(...data.data);
+  }
+
+  if (cards.length) {
+    console.log(`Set: ${setId} Total: ${cards.length}`);
+    await writeFile(
+      path.resolve(__dirname, "..", "cards", "en", `${setId}.json`),
+      JSON.stringify(
+        cards.map(({ tcgplayer: _, cardmarket: __, ...card }) => card),
+        undefined,
+        2
+      )
+    );
+  }
+};
+
+scrape(setId).then(() => process.exit(0));
